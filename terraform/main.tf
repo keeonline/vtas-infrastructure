9380845f4a067610b91f99952ec81dbc4ac231b8
keeonline <keeonline@outlook.com>
2023-10-22 11:30:30 +0100

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -125,58 +125,58 @@ resource "azurerm_linux_virtual_machine" "vtas_vm" {
 
 # Create the application host vm
 
-resource "azurerm_public_ip" "app_pip" {
-  name                = "keeonline-app-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Basic"
-}
+# resource "azurerm_public_ip" "app_pip" {
+#   name                = "keeonline-app-pip"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   allocation_method   = "Static"
+#   sku                 = "Basic"
+# }
 
 
-resource "azurerm_network_interface" "app_nic" {
-  name                = "keeonline-app-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_network_interface" "app_nic" {
+#   name                = "keeonline-app-nic"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "keeonline_app_nic_config"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.app_pip.id
-  }
-}
+#   ip_configuration {
+#     name                          = "keeonline_app_nic_config"
+#     subnet_id                     = azurerm_subnet.subnet.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.app_pip.id
+#   }
+# }
 
-resource "azurerm_linux_virtual_machine" "app_vm" {
-  name = "keeonline-app-vm"
-  computer_name = "app-host"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size = "Standard_B1s"
-  disable_password_authentication = false
-  admin_username = "adminuser"
-  admin_password = "avingAg1raffe!"
-  network_interface_ids = [azurerm_network_interface.app_nic.id]
+# resource "azurerm_linux_virtual_machine" "app_vm" {
+#   name = "keeonline-app-vm"
+#   computer_name = "app-host"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   size = "Standard_B1s"
+#   disable_password_authentication = false
+#   admin_username = "adminuser"
+#   admin_password = "avingAg1raffe!"
+#   network_interface_ids = [azurerm_network_interface.app_nic.id]
 
-  os_disk {
-    name = "keeonline-app-os-disk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     name = "keeonline-app-os-disk"
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts-gen2"
-    version   = "latest"
-  }
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-focal"
+#     sku       = "20_04-lts-gen2"
+#     version   = "latest"
+#   }
 
-  tags = {
-    ansible = "application"
-  }
+#   tags = {
+#     ansible = "application"
+#   }
 
-  custom_data = filebase64("scripts/application.sh")
-}
+#   custom_data = filebase64("scripts/application.sh")
+# }
 
 # Create the repository host vm
 
